b6e73ed5a8a3feef3315514d81b088e300e2838e
jamiebdavis <jamied1985@gmail.com>
2019-08-18 18:41:58 +0100

    updated background images

--- js/main.js
+++ js/main.js
@@ -42,16 +42,20 @@ function setBgGreet() {
 
   if (hour < 12) {
     //Morning
-    document.body.style.backgroundImage = "url(../img/morning.jpg)";
+
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/7vDLJFb/morning.jpg')";
     greeting.textContent = "Good Morning";
   }
   if (hour >= 12 || hour < 18) {
     //Afternoon
-    document.body.style.backgroundImage = "url(../img/afternoon.jpg)";
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/3mThcXc/afternoon.jpg')";
     greeting.textContent = "Good Afternoon";
   } else {
     //Evening
-    document.body.style.backgroundImage = "url(../img/night.jpg)";
+    document.body.style.backgroundImage =
+      "url('https://i.ibb.co/924T2Wv/night.jpg')";
     greeting.textContent = "Good Evening";
     document.body.style.color = "white";
   }
